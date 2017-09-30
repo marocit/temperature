edb1aaf2f516e4fc6f4ad71d9b8919f88f98ba48
Chris <cmaroska@marocit.de>
2017-09-30 02:31:01 +0200

    wwde

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Temperature;
+use App\Http\Resources\TemperatureCollection;
 
 class TemperatureController extends Controller
 {
